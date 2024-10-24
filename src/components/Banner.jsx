1ca07221e437dd07f74467d2fbe1ea0fefd21b92
mahamudul hasan seber <sebermh@gmail.com>
2024-10-25 00:30:36 +0600

    Banner-Section Done

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,8 +1,27 @@
 
 const Banner = () => {
     return (
-        <div>
-            Banner
+        <div className="flex items-center justify-center">
+            <div
+                className="hero   bg-black w-[1100px] h-[500px] rounded-xl"
+                style={{
+                    backgroundImage: `url(https://i.ibb.co.com/qj8VyvK/bg-shadow.png)`,
+                    backgroundSize: 'cover',
+                    backgroundPosition: 'center'
+
+                }}>
+
+                <div className="hero-content text-neutral-content text-center ">
+                    <div className="">
+                        <img className="lg:pl-48 pl-20 pb-8 " src="/public/img/banner-main.png" alt="" />
+                        <h1 className="mb-5 text-3xl font-bold ">Assemble Your Ultimate Dream 11 Cricket Team</h1>
+                        <p className="mb-5 text-gray-400">
+                            Beyond Boundaries Beyond Limits
+                        </p>
+                        <button className="btn btn-outline btn-warning   ">Claim Free Credit</button>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
