33d5343904417e3a579e1b28b4dd69a32c659792
mahamudul hasan seber <sebermh@gmail.com>
2024-10-25 01:08:53 +0600

    HButton-section Done

--- src/components/HButton.jsx
+++ src/components/HButton.jsx
@@ -1,8 +1,12 @@
 
 const HButton = () => {
     return (
-        <div>
-            H1 + button
+        <div className="flex justify-between pt-10 ">
+            <h1 className="w-62 h-9 font-bold text-3xl">Available Players</h1>
+            <div>
+                <button className="btn hover:bg-yellow-300 border border-yellow-900">Available</button>
+                <button className="btn hover:bg-yellow-300 border border-yellow-900">Selected (0)</button>
+            </div>
         </div>
     );
 };
