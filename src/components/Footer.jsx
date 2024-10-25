ef1202d44f6ae0ffecc47ba4cec6b57d1926acbb
mahamudul hasan seber <sebermh@gmail.com>
2024-10-25 21:48:40 +0600

    add code

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,14 +6,14 @@ const Footer = () => {
                 <div className="flex items-center justify-center pb-4 ">
                     <img src="/public/img/logo-footer.png" alt="" />
                 </div>
-                <div className="flex justify-around pt-8 pb-36">
+                <div className="lg:flex md:mb-10 justify-around pt-8 pb-36">
                     <nav>
-                        <h6 className="footer-title text-white ">About Us</h6>
+                        <h6 className="footer-title text-white  ">About Us</h6>
                         <p className="text-gray-600">We are a passionate team dedicated to providing <br />
                             the best services to our customers.</p>
                     </nav>
                     <nav>
-                        <h6 className="footer-title text-white">Quick Links</h6>
+                        <h6 className="footer-title text-white ">Quick Links</h6>
                         <li><a className="link link-hover text-gray-600">Home</a></li>
                         <li><a className="link link-hover text-gray-600">Services</a></li>
                         <li><a className="link link-hover text-gray-600">About </a></li>
